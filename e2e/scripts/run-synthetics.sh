7c431a4a4b1342988b8a079ae0f8b871ea1f21fa
roman.gaignault <roman.gaignault@datadoghq.com>
2026-03-11 15:20:18 +0100

    Syntax correction

--- e2e/scripts/run-synthetics.sh
+++ e2e/scripts/run-synthetics.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eo pipefail
+set -e -o pipefail
 
 # Run Synthetics tests for a given platform.
 #
--- e2e/scripts/upload.sh
+++ e2e/scripts/upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eo pipefail
+set -e -o pipefail
 
 # Upload app binary to Datadog Synthetics.
 #
