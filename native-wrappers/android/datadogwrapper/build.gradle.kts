5b376decd0de0bdfcd37e869682eb8da6195aae5
Jeff Ward <jeff.ward@datadoghq.com>
2026-05-04 09:44:35 -0400

    Update to Android to 3.9.1

--- native-wrappers/android/datadogwrapper/build.gradle.kts
+++ native-wrappers/android/datadogwrapper/build.gradle.kts
@@ -35,12 +35,12 @@ android {
 dependencies {
     // Use 'implementation' to hide dependencies from AAR consumers
     // Dependencies will be needed at runtime but won't be in the binding API
-    implementation("com.datadoghq:dd-sdk-android-core:3.9.0")
-    implementation("com.datadoghq:dd-sdk-android-logs:3.9.0")
-    implementation("com.datadoghq:dd-sdk-android-trace:3.9.0")
-    implementation("com.datadoghq:dd-sdk-android-rum:3.9.0")
-    implementation("com.datadoghq:dd-sdk-android-ndk:3.9.0")
-    implementation("com.datadoghq:dd-sdk-android-session-replay:3.9.0")
+    implementation("com.datadoghq:dd-sdk-android-core:3.9.1")
+    implementation("com.datadoghq:dd-sdk-android-logs:3.9.1")
+    implementation("com.datadoghq:dd-sdk-android-trace:3.9.1")
+    implementation("com.datadoghq:dd-sdk-android-rum:3.9.1")
+    implementation("com.datadoghq:dd-sdk-android-ndk:3.9.1")
+    implementation("com.datadoghq:dd-sdk-android-session-replay:3.9.1")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
     // Test dependencies
