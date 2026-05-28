5bacbe986512369ddecef7c2e3efcd9473780a23
Sergio Barrio <sergio.barrio@datadoghq.com>
2026-05-28 12:23:03 +0200

    Update Copyrights

--- native-wrappers/ios/build.sh
+++ native-wrappers/ios/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
 # This product includes software developed at Datadog (https://www.datadoghq.com/)
-# Copyright 2026 Datadog, Inc.
+# Copyright 2026 - Present Datadog, Inc.
 set -e
 
 echo "🔨 Building DatadogWrapper XCFramework..."
